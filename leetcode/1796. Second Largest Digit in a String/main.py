9dcc9c879ee691839cebe4ab5250d8c17fa4c3a3
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2021-03-25 09:55:55 +0600

    leetcode #1796

--- leetcode/1796. Second Largest Digit in a String/main.py
+++ leetcode/1796. Second Largest Digit in a String/main.py
@@ -1,10 +1,10 @@
 class Solution:
     def secondHighest(self, original_string: str) -> int:
-        digits = set()
+        basket = set()
         for character in original_string:
             if character.isdigit():
-                digits.add(int(character))
-        if len(digits) < 2:
-            return -1
+                basket.add(int(character))
+        if len(basket) > 1:
+            return sorted(basket)[-2]
         else:
-            return sorted(digits)[-2]
+            return -1
