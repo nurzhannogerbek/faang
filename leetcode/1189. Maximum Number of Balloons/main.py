b02f87372a267fcc7d2393cfd2036acb8f77c0bf
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2021-03-28 17:27:24 +0600

    leetcode #1189

---
+++ leetcode/1189. Maximum Number of Balloons/main.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        counter = Counter(text)
+        return min(counter['b'], counter['a'], counter['l'] // 2, counter['o'] // 2, counter['n'])
