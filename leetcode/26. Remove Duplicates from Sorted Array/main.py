73a33c95fc7035504e078a627eec7c44b9333b65
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2023-02-20 17:05:22 +0600

    leetcode #26

---
+++ leetcode/26. Remove Duplicates from Sorted Array/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # Two indexes approach.
+        # First Index is responsible for writing unique values in our input array, while Second Index will read the input array and pass all the distinct elements to First Index.
+        size = len(nums)
+        insert_index = 1
+        for i in range(1, size):
+            # Found unique element.
+            if nums[i - 1] != nums[i]:
+                # Updating insertIndex in our main array.
+                nums[insert_index] = nums[i]
+                # Incrementing insertIndex count by 1.
+                insert_index += 1
+        return insert_index
